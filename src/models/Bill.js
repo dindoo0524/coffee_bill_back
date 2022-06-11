9654125555f436571114f48db44decab528c42c0
나진주 <jinju@liveklass.com>
2022-06-11 22:08:03 +0900

    Bill 모델 수정

--- src/models/Bill.js
+++ src/models/Bill.js
@@ -13,34 +13,19 @@ class Bill {
         const conn = await db.connection()
         const result = await conn.execute('SELECT count(*) cnt FROM bills where seq = ?', [billSeq])
         const count = result[0][0].cnt
-        return count
+        return count > 0
     }
 
     static async finishBill(billSeq) {
         const conn = await db.connection()
         await conn.execute('UPDATE bills SET status = 1, endTime = now() where seq = ?', [billSeq]);
+    }
+
+    static async getBill(billSeq) {
+        const conn = await db.connection()
         const [rows] = await conn.execute('SELECT * FROM bills where seq = ?', [billSeq])
         return rows
     }
-
-    // static async getOrders(billSeq, drinkSeq) {
-    //     const conn = await db.connection()
-    //     let result;
-
-    //     if (drinkSeq) {
-    //         result = await conn.execute('SELECT * FROM orders where billSeq = ? and drinkSeq = ?', [billSeq, drinkSeq]);
-    //     } else {
-    //         result = await conn.execute('SELECT drinkSeq, drinkType, count(*) cnt FROM orders where billSeq = ? group by drinkSeq, drinkType', [billSeq]);
-    //     }
-    //     return result[0]
-    // }
-
-    // static async addOrder(billSeq, data) {
-    //     const { drinkSeq, nickname, drinkType, optionDescription } = data;
-    //     const record = [billSeq, drinkSeq, nickname, drinkType, optionDescription]
-    //     const conn = await db.connection()
-    //     await conn.execute('INSERT INTO orders(billSeq, drinkSeq, nickname, drinkType, optionDescription ) VALUES (?, ?, ?, ?, ?)', record);
-    // }
 }
 
 module.exports = Bill
