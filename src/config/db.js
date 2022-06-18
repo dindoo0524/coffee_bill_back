baba55d93ab78680b9f8999f77388d4984cb8d5a
나진주 <jinju@liveklass.com>
2022-06-18 22:19:57 +0900

    mysql host name 환경변수

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 
 const connection = async () =>
     mysql.createConnection({
-        host     : 'localhost',
+        host     : process.env.HOST,
         user     : process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: 'coffee_bill'
