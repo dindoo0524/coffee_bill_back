cb19e4c78ddbe4424788b7caa504c2b1dd46497f
나진주 <jinju@liveklass.com>
2022-06-18 19:03:56 +0900

    에러메세지 영문 버전

--- src/routes/bills/ctrl.js
+++ src/routes/bills/ctrl.js
@@ -26,11 +26,11 @@ const finishBill = async (req, res) => {
 
 const getOrders = async (req, res) => {
   const { billSeq } = req.params;
-  const drinkSeq = req.query.drinkSeq
+  const { drinkSeq, drinkType } = req.query
   
   let orders
   if (drinkSeq) {
-    orders = await Order.getDrinkOrders(billSeq, drinkSeq)
+    orders = await Order.getDrinkOrders(billSeq, drinkSeq, drinkType)
   } else {
     orders = await Order.getAllOrders(billSeq)
   }
@@ -58,7 +58,7 @@ const checkBill = async (req, res, next) => {
   // billSeq 가 유효한 값인지 check
   const exist = await Bill.checkBill(billSeq)
   if (!exist) {
-    res.status(400).json({ message: '존재하지 않는 주문서 입니다' })
+    res.status(400).json({ message: 'Bill does not exist' })
     return
   }
   next()
@@ -68,10 +68,9 @@ const checkAuth = async (req, res, next) => {
   const { billSeq } = req.params
   const { nickname } = req.body
 
-  // billSeq 가 유효한 값인지 check
   const auth = await Bill.checkAuth(billSeq, nickname)
   if (!auth) {
-    res.status(400).json({ message: '권한이 없습니다' })
+    res.status(400).json({ message: 'Authentication failed ' })
     return
   }
   next()
