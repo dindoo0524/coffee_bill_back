9622352cbc228d9adcb94ab7b26fe8e68d9f30ee
나진주 <jinju@liveklass.com>
2022-12-26 23:14:03 +0900

    테스트 로그

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,14 @@ const mysql = require('mysql2/promise');
 
 const connection = async () =>
     mysql.createConnection({
-        host     : process.env.HOST,
-        user     : process.env.MYSQL_USER,
+        host: process.env.HOST,
+        port: 3306,
+        user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: 'coffee_bill'
     });
+
+    console.log(process.env.HOST, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD)
 
 
 module.exports = {
